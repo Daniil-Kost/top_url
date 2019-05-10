cc4f07e58f979486c95a684adcdbdddc95fd5b9b
Daniil <dkosty@softserveinc.com>
2019-05-10 12:41:34 +0300

    Edit some code in db_setup

--- db_setup.py
+++ db_setup.py
@@ -5,9 +5,12 @@ import asyncio
 async def check_user_tables_existing():
     query = "SELECT 'public.app_user', 'public.user_urls'::regclass"
     result = await db_conn.raw_query(query)
-    exists = len(result[0])
-    if not 'public.app_user' and 'user_urls' in exists:
+    if not 'public.app_user' and 'user_urls' in result[0]:
         await create_user_tables()
+    elif 'public.app_user' in result[0] and 'user_urls' not in result[0]:
+        print(f"User table: 'user_urls' does not exist in DB")
+    elif 'user_urls' in result[0] and 'public.app_user' not in result[0]:
+        print(f"User table: 'public.app_user' does not exist in DB")
     else:
         print(f"User tables: {result[0]} exists in DB")
 
